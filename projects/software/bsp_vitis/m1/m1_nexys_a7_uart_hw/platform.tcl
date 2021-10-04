d560256fec196875b8ba96285d3af7d99f3290cd
Jose Pablo Castro Valverde <josepablo.castro@yumbaruti.org>
2021-10-04 12:08:05 -0600

    Rename project from m1_nexys_uart_hw to m1_nexys_a7_uart_hw

--- projects/software/bsp_vitis/m1/m1_nexys_a7_uart_hw/platform.tcl
+++ projects/software/bsp_vitis/m1/m1_nexys_a7_uart_hw/platform.tcl
@@ -1,20 +1,20 @@
 # 
 # Usage: To re-create this platform project launch xsct with below options.
-# xsct /home/jose/Documents/git-ws/personal/local/M1_Nexys_SandBox/projects/m1/software/m1_nexys_a7_uart/sdk_worspace/m1_nexys_uart_hw/platform.tcl
+# xsct /home/jose/Documents/git-ws/personal/local/M1_Nexys_SandBox/projects/m1/software/m1_nexys_a7_uart/sdk_worspace/m1_nexys_a7_uart_hw/platform.tcl
 # 
 # OR launch xsct and run below command.
-# source /home/jose/Documents/git-ws/personal/local/M1_Nexys_SandBox/projects/m1/software/m1_nexys_a7_uart/sdk_worspace/m1_nexys_uart_hw/platform.tcl
+# source /home/jose/Documents/git-ws/personal/local/M1_Nexys_SandBox/projects/m1/software/m1_nexys_a7_uart/sdk_worspace/m1_nexys_a7_uart_hw/platform.tcl
 # 
 # To create the platform in a different location, modify the -out option of "platform create" command.
 # -out option specifies the output directory of the platform project.
 
-platform create -name {m1_nexys_uart_hw}\
+platform create -name {m1_nexys_a7_uart_hw}\
 -hw {/home/jose/Documents/git-ws/personal/local/M1_Nexys_SandBox/projects/m1/hardware/m1_nexys_a7_uart/m1_uart_wrapper.xsa}\
 -proc {CORTEXM1_AXI_0} -os {standalone} -fsbl-target {psu_cortexa53_0} -out {/home/jose/Documents/git-ws/personal/local/M1_Nexys_SandBox/projects/m1/software/m1_nexys_a7_uart/sdk_worspace}
 
 platform write
 platform generate -domains 
-platform active {m1_nexys_uart_hw}
+platform active {m1_nexys_a7_uart_hw}
 bsp reload
 platform generate
 platform clean
@@ -22,7 +22,7 @@ platform generate
 platform config -updatehw {/home/jose/Documents/git-ws/personal/local/M1_Nexys_SandBox/projects/m1/hardware/m1_nexys_a7_uart/m1_uart_wrapper.xsa}
 platform generate -domains standalone_domain 
 bsp reload
-platform active {m1_nexys_uart_hw}
+platform active {m1_nexys_a7_uart_hw}
 bsp reload
 platform clean
 platform config -updatehw {/home/jose/Documents/git-ws/personal/local/M1_Nexys_SandBox/projects/m1/hardware/m1_nexys_a7_uart/m1_uart_wrapper.xsa}
